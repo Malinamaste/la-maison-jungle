c3d6a89d19f87f87fe784f0cac08dca8002f8c53
Melanie Morey <morey.melanie@gmail.com>
2023-03-08 18:35:36 +0100

    firt commit

---
+++ src/components/Banner.js
@@ -0,0 +1,8 @@
+import '../styles/Banner.css'
+// import Recommendation from '../Recommendation.js'
+
+function Banner({ children }) {
+  return <div className='lmj-banner'>{children}</div>
+}
+
+export default Banner
